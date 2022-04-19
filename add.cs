0831ff4e267cccac66657662b8b9c5c9a7d0c685
sana-sk <98468429+sana-sk@users.noreply.github.com>
2022-04-19 11:06:49 +0530

    Update add.cs

--- add.cs
+++ add.cs
@@ -9,7 +9,9 @@ namespace MyApplication
       int x = 5;
       int y = 6;
       int sum = x + y;
-      Console.WriteLine(sum); // Print the sum of x + y
+      Console.WriteLine(sum);
+      
+      // Print the sum of x + y
     }
   }
 }
